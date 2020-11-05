0f7722a32f01233bf4ce0d92578564a8c8b6f5e2
PatrickSchwab <patrick.schwab@lernende.bbw.ch>
2020-11-05 11:15:54 +0100

    ImpossibleBottleNeckValue changed to ImpossibleBottleNeckValueException

---
+++ fordFulkerson/src/main/java/test/MaxFlowTest.java
@@ -0,0 +1,21 @@
+package test;
+
+/**
+ * @author Paede
+ * @version 05.11.2020
+ */
+
+public class MaxFlowTest {
+
+    void setUp() {
+    }
+
+    void initData() {
+    }
+
+    void pointToEdges() {
+    }
+
+    void maxFlow() {
+    }
+}
