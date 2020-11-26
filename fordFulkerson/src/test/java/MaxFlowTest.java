9512ba708ac9d7cf0f682d9b16289943c79b086f
PatrickSchwab <patrick.schwab@lernende.bbw.ch>
2020-11-26 11:07:44 +0100

    cleanup

--- fordFulkerson/src/test/java/MaxFlowTest.java
+++ fordFulkerson/src/test/java/MaxFlowTest.java
@@ -2,7 +2,6 @@ import exception.ImpossibleBottleNeckValueException;
 import exception.ImpossibleOrderException;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.ArrayList;
